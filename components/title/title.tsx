8f2c00ffffc2e04df82d91afed765c7a29eba7a8
Cooper Werner <cooperwerner824@gmail.com>
2024-02-11 21:07:29 -0500

    Title is good enough

--- components/title/title.tsx
+++ components/title/title.tsx
@@ -1,14 +1,17 @@
 import styles from "./title.module.css"
 export default function Title(){
     return(
-        <div className= {styles.Title}>
-            <div className= {styles.TitleLeft}> 
-                <img className= "TitleLeftImg "src="hackrpiLogo.png" alt="HackRPI Logo"/> 
+        <div className={styles.titleContainer}>
+            <div className={styles.leftContainer}>
+                <img className={styles.titleImg} src="hackrpiLogo.png" alt="logo" />
             </div>
-
-            <div className= {styles.TitleRight}> 
+            <div className={styles.rightContainer}>
+                <h1 className={styles.title}>HackRPI</h1>
+                <p className={styles.subtitle}>
+                RPI's annual hackathon. Join us for 24 hours 
+                of fun in the fall.
+                </p>
             </div>
-    
         </div>
     )
 }

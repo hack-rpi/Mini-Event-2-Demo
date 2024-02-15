fa2fc3d5c99ddbfc02368088a3ffa62659115889
Cooper Werner <cooperwerner824@gmail.com>
2024-02-15 17:38:18 -0500

    Stylistic Changes

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -2,20 +2,13 @@ import styles from "./about.module.css";
 export default function About() {
   return (
     <div className={styles.aboutContainer} id="about">
-      <h1 className={styles.aboutTitle}>About Mr. Beast</h1>
-      <p className={styles.aboutText}>
-        Mr. Beast is a company that specializes in creating events for the
-        community. We are dedicated to creating events that are fun, engaging,
-        and educational. We believe that everyone should have the opportunity to
-        learn and grow. Our events are designed to help people connect, learn,
-        and have fun.
+      <h1 className={styles["aboutTitle"]}>About</h1>
+      <p className={styles["aboutText"]}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
       </p>
-      <p className={styles.topVideosLabel}>This year's top videos </p>
-      <ul className={styles.topVideos}>
-        <li>1. Giving $100,000 To A Homeless Person</li>
-        <li>2. I Spent 24 Hours Straight In Insane Asylum</li>
-        <li>3. I Gave $500,000 To Random People</li>
-      </ul>
     </div>
   );
 }
